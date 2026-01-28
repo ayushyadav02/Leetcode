90b32122ac795dac66d0f65b3ad5e5b6ea718e34
Ayush yadav <149177315+ayushyadav02@users.noreply.github.com>
2026-01-28 10:37:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (84.71%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int l = 0, r = x;
+
+        while (l <= r) {
+            long long mid = l + (r - l) / 2;
+
+            if (mid * mid == x) {
+                return mid;
+            } 
+            else if (mid * mid < x) {
+                l = mid + 1;
+            } 
+            else {
+                r = mid - 1;
+            }
+        }
+        return r;   // r is the integer square root
+    }
+};
