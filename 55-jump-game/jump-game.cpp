614391e2d428b6ef8547f1eadcd261f528b89400
Ayush yadav <149177315+ayushyadav02@users.noreply.github.com>
2026-02-18 12:52:24 +0530

    Time: 266 ms (13.40%) | Memory: 53.2 MB (14.55%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        vector<bool> dp(n, false);
+
+  
+        dp[n - 1] = true;
+
+        for (int i = n - 2; i >= 0; i--) {
+            int maxJump = nums[i];
+            for (int j = 1; j <= maxJump && i + j < n; j++) {
+                if (dp[i + j]) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[0];
+    }
+};
